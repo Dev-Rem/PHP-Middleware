5318fd7f60c76565eb52c83ba1537051d626e626
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2021-01-20 18:13:26 +0100

    Update .gitignore

--- php/db_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $db = "users";
-    $dbpass = "dbpass";
-
-    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-?>
